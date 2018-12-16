4dc3207f08d751d88b44f2a58c51430f34e0bd40
Mohammad Al-Ahdal <mohammad@enggmomo.me>
2018-12-16 04:52:59 -0700

    added photo gallery, fixed favicon, and fixed header

--- src/Screens/PhotographyScreen/PhotographyScreen.js
+++ src/Screens/PhotographyScreen/PhotographyScreen.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
+import Gallery from 'react-photo-gallery'
+
 class PhotographyScreen extends React.Component {
     render(){
         return(
             <div className="photographyScreen">
-
+                <div className="headerSpacing"></div>
+                <Gallery photos={PHOTO_SET}/>
             </div>
         );
     }
 }
+
+const PHOTO_SET = [
+    // {
+    //     src: 'https://picsum.photos/1200/900',
+    //     width: 1200,
+    //     height: 900,
+    // }
+]
 
 export default PhotographyScreen
