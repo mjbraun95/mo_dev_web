0a536263beca36837fa1144c5a8a4030f67223fd
Mohammad Al-Ahdal <mohammad@enggmomo.me>
2018-12-17 15:35:46 -0700

    added error page

--- src/Screens/Error404/Error404.js
+++ src/Screens/Error404/Error404.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-class Error404 extends React.Component {
-    render(){
-        return(
-            <div className="error404">
+import { Link } from 'react-router-dom'
 
+import './error404.scss'
+import logo from '../../assets/logo.png'
+
+class Error404 extends React.Component {
+    render() {
+        return (
+            <div className="error404">
+                <Link to="/"><img src={logo} alt="Click to go Home" /></Link>
+                <h1>Error 404: Page not found</h1>
+                <h3>Looks like we've managed to wander off the path. Let's head back <Link to="/">home</Link></h3>
             </div>
         );
     }
