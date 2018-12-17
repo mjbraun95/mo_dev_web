c8ffe3ceb62164de44f5054a1427a1dc5e63d3d5
Mohammad Al-Ahdal <mohammad@enggmomo.me>
2018-12-17 15:28:41 -0700

    added temporary portfolio screen

--- src/Screens/PortfolioScreen/PortfolioScreen.js
+++ src/Screens/PortfolioScreen/PortfolioScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import './portfolioScreen.scss'
 class PortfolioScreen extends React.Component {
     render(){
         return(
             <div className="portfolioScreen">
-
+                <h1>Coming soon!</h1>
+                <h3>This page is currently under construction! It should be available in early 2019!</h3>
             </div>
         );
     }
