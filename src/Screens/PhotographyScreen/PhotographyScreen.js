3f5f4852a0d1350214168f838197991f568bf2ab
Mohammad Al-Ahdal <mohammad@enggmomo.me>
2018-12-19 22:49:03 -0700

    added left-right key press carousel on photography and updated collection

--- src/Screens/PhotographyScreen/PhotographyScreen.js
+++ src/Screens/PhotographyScreen/PhotographyScreen.js
@@ -10,9 +10,11 @@ class PhotographyScreen extends React.Component {
         photo: {
             src: null,
             width: 0,
-            height: 0
+            height: 0,
+            index: 0
         }
     }
+
     render() {
         return (
             <div className="photographyScreen">
@@ -20,10 +22,39 @@ class PhotographyScreen extends React.Component {
                     className="headerSpacing">
                     <h1>Photography</h1>
                 </div>
-                <Gallery onClick={(e, obj) => { this.setState({ photo: obj.photo, box: true }) }} photos={PHOTO_SET} />
+                <Gallery onClick={(e, obj) => { this.setState({ photo: { ...obj.photo, index: obj.index }, box: true }) }} photos={PHOTO_SET} />
                 {
                     this.state.box ?
-                        <div onClick={() => { this.setState({ box: false }) }} className="boxContainer">
+                        <div
+                            ref={(box) => {
+                                this.photoBox = box;
+                                this.state.box && this.photoBox ? this.photoBox.focus() : console.log("Component Unmounted");
+                            }}
+                            tabIndex="-1"
+                            onKeyDown={(event) => {
+                                if (event.key === "ArrowLeft") {
+                                    let newIndex;
+                                    if (this.state.photo.index === 0) {
+                                        newIndex = PHOTO_SET.length - 1;
+                                    } else {
+                                        newIndex = this.state.photo.index - 1
+                                    }
+                                    let newPhoto = PHOTO_SET[newIndex];
+                                    this.setState({ photo: { ...newPhoto, index: newIndex } })
+                                } else if (event.key === "ArrowRight") {
+                                    let newIndex;
+                                    if (this.state.photo.index === PHOTO_SET.length - 1) {
+                                        newIndex = 0;
+                                    } else {
+                                        newIndex = this.state.photo.index + 1
+                                    }
+                                    let newPhoto = PHOTO_SET[newIndex];
+                                    this.setState({ photo: { ...newPhoto, index: newIndex } })
+                                }
+
+                            }}
+                            onClick={() => { this.photoBox.blur(); this.setState({ box: false }); }}
+                            className="boxContainer">
                             <img src={this.state.photo.src} alt={this.state.photo.src} />
                         </div>
                         : null
@@ -35,14 +66,19 @@ class PhotographyScreen extends React.Component {
 
 const PHOTO_SET = [
     {
+        src: 'https://instagram.fyxd1-1.fna.fbcdn.net/vp/27ac4caccf121c0590ead238719d7af3/5C95950A/t51.2885-15/sh0.08/e35/s640x640/47151064_371762386960877_9158180463179330915_n.jpg?_nc_ht=instagram.fyxd1-1.fna.fbcdn.net',
+        width: 1080,
+        height: 720,
+    },
+    {
+        src: 'https://instagram.fyxd1-1.fna.fbcdn.net/vp/a9a19e3de4bd0fce7bd1a0bbcdee7676/5CB851F1/t51.2885-15/sh0.08/e35/s640x640/46925662_215629462647064_7895917795838685729_n.jpg?_nc_ht=instagram.fyxd1-1.fna.fbcdn.net',
+        width: 1080,
+        height: 695,
+    },
+    {
         src: 'https://scontent-yyz1-1.cdninstagram.com/vp/fbf5e6566b90e4a8c589bc2a80baa44d/5CA8B7CF/t51.2885-15/sh0.08/e35/s640x640/46645051_351551815634798_6936702293998497003_n.jpg?_nc_ht=scontent-yyz1-1.cdninstagram.com',
         width: 600,
         height: 337,
-    },
-    {
-        src: 'https://scontent-yyz1-1.cdninstagram.com/vp/031bbb384780bf486fe702de9283c8c5/5C9DD584/t51.2885-15/sh0.08/e35/s640x640/41605384_250400465676689_2464122992578708258_n.jpg?_nc_ht=scontent-yyz1-1.cdninstagram.com',
-        width: 600,
-        height: 449,
     },
     {
         src: 'https://scontent-yyz1-1.cdninstagram.com/vp/7f9b37dae7abaaad1c94370e06bde88d/5CA05DF2/t51.2885-15/sh0.08/e35/s640x640/41792081_1123897701095288_5673133217950828794_n.jpg?_nc_ht=scontent-yyz1-1.cdninstagram.com',
@@ -68,11 +104,6 @@ const PHOTO_SET = [
         src: 'https://scontent-yyz1-1.cdninstagram.com/vp/3d73f312bacfcb33789a5685a32680b1/5C8FE66D/t51.2885-15/sh0.08/e35/s640x640/39504466_457406511438241_1286961691543732224_n.jpg',
         width: 1080,
         height: 809,
-    },
-    {
-        src: 'https://scontent-yyz1-1.cdninstagram.com/vp/414deb08c6da2d58dc2964e4df557815/5C92A8B5/t51.2885-15/sh0.08/e35/s640x640/41750032_419768688552902_1663548051059900997_n.jpg?_nc_ht=scontent-yyz1-1.cdninstagram.com',
-        width: 1080,
-        height: 808,
     },
 ]
 
