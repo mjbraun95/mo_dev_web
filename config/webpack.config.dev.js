6e24827237e6c2a52fb74e2bfc65949a41c1f092
Mohammad Al-Ahdal <mohammad@enggmomo.me>
2019-03-01 03:02:38 -0700

    add custom paths

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -150,6 +150,8 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      'mos-components': path.resolve(__dirname, '../src/Components'),
+      'mos-screens': path.resolve(__dirname, '../src/Screens')
     },
     plugins: [
       // Adds support for installing with Plug'n'Play, leading to faster installs and adding
