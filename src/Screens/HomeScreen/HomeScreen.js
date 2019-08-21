d3b616093c812002d393aae9a5b73390b6e55d4a
Mohammad Al-Ahdal <mo@mohammad.dev>
2019-08-21 02:35:28 -0600

    update homescreen

--- src/Screens/HomeScreen/HomeScreen.js
+++ src/Screens/HomeScreen/HomeScreen.js
@@ -41,18 +41,20 @@ class HomeScreen extends React.Component {
                     <h1>Hi, I'm Mohammad!</h1>
 
                     <h3>I'm a <b>Web Developer</b>,</h3>
-                    <h3><b>Mobile App Developer</b>,</h3>
-                    <h3><b>Server Guru</b>,</h3>
+                    <h3>a <b>Mobile App Developer</b>,</h3>
+                    <h3>a <b>Server Guru</b>,</h3>
+                    <h3>an <b>Amateur Photographer</b>,</h3>
                     <h3>and a <b>Nerd</b></h3>
+                    <h6>(oh and I'm also completely self-taught)</h6>
 
                     <p>
                         I started coding {moment().diff(moment("20110214"), "years")} years ago when I first came to Canada from Libya. Since then, I've been working on a whole lot of different projects - cool stuff that makes me happy. My first project was to make my mom a "Happy Mother's Day" application. That soon ramped up into building robots on the <a href="https://www.firstinspires.org/robotics/frc">FRC</a> teams 4719, 4627, and Ri3D T.E.S.L.A. If you want to check out some of my more recent projects, checkout my <Link to="/portfolio">portfolio</Link>!
                     </p>
                     <p>
-                        I currently study Electrical Engineering at <a href="https://uab.ca/">the University of Alberta</a> in the tradional program. I am currently set to graduate in April of 2021 after which I will be availble for full time employment in the area of Software Engineering.
+                        I have previously held Software Engineering positions namely a Software Consultant Intern at <a href="https://devfacto.com/">DevFacto</a>. I am currently working at <a href="https://intuit.com/">Intuit</a> as a Software Development Co-Op Intern with my contract set to end on December 23, 2019.
                     </p>
                     <p>
-                        I have previously held Software Engineering positions namely a Software Consultant Intern at <a href="https://devfacto.com/">DevFacto</a>. I am currently working at <a href="https://intuit.com/">Intuit</a> as a Software Development Co-Op Intern.
+                        My main area of focus is Web and Mobile Applications. I love designing and building captivating User Interfaces whilst delivering second to none User Experiences. I'm a Full Stack Developer with a preference to more front-end work. I've had close to 2 years of intense React/React Native exposure and I'd be lying if I said I didn't absolutely adore using React and React Native.
                     </p>
                     <p>
                         If you have any questions or would like to talk to me about anything, feel free to shoot me an email at <a href="mailto:mo@mohammad.dev">mo@mohammad.dev</a>!
